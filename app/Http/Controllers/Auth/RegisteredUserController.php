a2ccaa1f25b07dff3d014cbec8f66f0d2d517902
David Adokuru <davidadokuru@gmail.com>
2022-11-02 18:28:13 +0100

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,10 +39,31 @@ class RegisteredUserController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        $length = 26;
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        $number = $randomString;
+
+        if ($request->has('referral_code')) {
+            $referral_code = $request->input('referral_code');
+            $referral_user = User::where('referral_code', $referral_code)->first();
+            if ($referral_user) {
+                $referral_user->circle_points = $referral_user->circle_points + 5;
+                $referral_user->save();
+            }
+        }
+
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'referral_id' => $request->referral_id ?? null,
+            'referral_code' => substr($request->name, 0, 3) . substr($number, 0, 3),
         ]);
 
         event(new Registered($user));
